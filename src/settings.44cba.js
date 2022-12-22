5d0d18e763e9a71178dbe9fd234b54ff95563575
AnarkiXD <siso7667@gmail.com>
2022-12-22 16:42:33 +0800

    A

---
+++ src/settings.44cba.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/resources/Scenes/Loading.fire",orientation:"landscape",jsList:["assets/Script/Libraries/CryptoJS/core-min.dda48.js"],bundleVers:{internal:"52d36",resources:"65e9d",main:"d0a50"}};
